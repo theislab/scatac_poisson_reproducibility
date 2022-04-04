e9dba3522130795ef99d4a3c4272625e06deb9ed
Laura Martens <laura.d.martens@icloud.com>
2022-04-04 19:46:15 +0200

    Update scripts

--- poisson_atac/scripts/cistopic/convert_adata_seurat.R
+++ poisson_atac/scripts/cistopic/convert_adata_seurat.R
@@ -11,8 +11,10 @@ path <- "/storage/groups/ml01/workspace/laura.martens/atac_poisson_data/data/neu
 batches = c('s1d1','s1d2','s1d3','s2d1','s2d4','s2d5','s3d10','s3d3','s3d6','s3d7')
 for (batch in batches) {
     print(batch)
-   Convert(paste0(path, "openproblems_bmmc_multiome_phase2.manual_formatting.output_mod2_lean_", batch, ".h5ad"), 
-paste0(path, "openproblems_bmmc_multiome_phase2.manual_formatting.output_mod2_", batch, ".h5seurat"), assay="ATAC")
+#    Convert(paste0(path, "openproblems_bmmc_multiome_phase2.manual_formatting.output_mod2_lean_", batch, ".h5ad"), 
+# paste0(path, "openproblems_bmmc_multiome_phase2.manual_formatting.output_mod2_", batch, ".h5seurat"), assay="ATAC")
+   Convert(paste0(path, "openproblems_bmmc_multiome_phase2.manual_formatting.output_mod2_lean_", batch, "_counts.h5ad"), 
+paste0(path, "openproblems_bmmc_multiome_phase2.manual_formatting.output_mod2_", batch, "_counts.h5seurat"), assay="ATAC")
 }
 
 
--- poisson_atac/scripts/cistopic/save_adata_no_obsm.py
+++ poisson_atac/scripts/cistopic/save_adata_no_obsm.py
@@ -1,24 +1,48 @@
 import poisson_atac as patac
 import os
 
+# path = "/storage/groups/ml01/workspace/laura.martens/atac_poisson_data/data/neurips/phase2-private-data/common/openproblems_bmmc_multiome_phase2/"
+# adata = patac.data.load_neurips()
+# adata.obsm = {}
+# adata.uns = {}
+# adata.layers = {}
+
+# adata.write(os.path.join(path, "openproblems_bmmc_multiome_phase2.manual_formatting.output_mod2_lean.h5ad"))
+
+# for batch in adata.obs.batch.unique():
+#     print(batch)
+#     a_batch = adata[adata.obs.batch == batch].copy()
+#     a_batch.write(os.path.join(path, f"openproblems_bmmc_multiome_phase2.manual_formatting.output_mod2_lean_{batch}.h5ad"))
+
+
+# path = "/storage/groups/ml01/workspace/laura.martens/atac_poisson_data/data/GSE129785_scATAC-Hematopoiesis/"
+# adata = patac.data.load_hematopoiesis()
+# adata.obsm = {}
+# adata.uns = {}
+# adata.layers = {}
+
+# adata.write(os.path.join(path, "GSE129785_scATAC-Hematopoiesis_lean.h5ad"))
+
 path = "/storage/groups/ml01/workspace/laura.martens/atac_poisson_data/data/neurips/phase2-private-data/common/openproblems_bmmc_multiome_phase2/"
 adata = patac.data.load_neurips()
-adata.obsm = {}
-adata.uns = {}
-adata.layers = {}
 
-adata.write(os.path.join(path, "openproblems_bmmc_multiome_phase2.manual_formatting.output_mod2_lean.h5ad"))
+adata.write(os.path.join(path, "openproblems_bmmc_multiome_phase2.manual_formatting.output_mod2_lean_counts.h5ad"))
 
 for batch in adata.obs.batch.unique():
     print(batch)
     a_batch = adata[adata.obs.batch == batch].copy()
-    a_batch.write(os.path.join(path, f"openproblems_bmmc_multiome_phase2.manual_formatting.output_mod2_lean_{batch}.h5ad"))
+    a_batch.X = a_batch.layers["counts"]
+    a_batch.obsm = {}
+    a_batch.uns = {}
+    a_batch.layers = {}
+    a_batch.write(os.path.join(path, f"openproblems_bmmc_multiome_phase2.manual_formatting.output_mod2_lean_{batch}_counts.h5ad"))
 
 
 path = "/storage/groups/ml01/workspace/laura.martens/atac_poisson_data/data/GSE129785_scATAC-Hematopoiesis/"
 adata = patac.data.load_hematopoiesis()
+adata.X = adata.layers["counts"]
 adata.obsm = {}
 adata.uns = {}
 adata.layers = {}
 
-adata.write(os.path.join(path, "GSE129785_scATAC-Hematopoiesis_lean.h5ad"))
+adata.write(os.path.join(path, "GSE129785_scATAC-Hematopoiesis_lean_counts.h5ad"))
