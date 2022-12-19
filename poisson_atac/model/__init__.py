5d43c8501afad43ab67aff6bbd8dcd6c11d4e557
Laura Martens <laura.d.martens@icloud.com>
2022-12-19 12:05:19 +0100

    Remove gextoatac prediction

--- poisson_atac/model/__init__.py
+++ poisson_atac/model/__init__.py
@@ -1,5 +1,3 @@
 from ._poissonvi import PoissonVI
-from ._poissongextoatacvi import GEXtoATAC
 from ._binaryvi import BinaryVI
-from ._binarygextoatacvi import BinaryGEXtoATAC
 from ._poissonmultivi import MULTIVI
